c6b3582480048267054e234ffde3a711206a34cf
Zeeshan Arshad <105277703+zeshanarshad28@users.noreply.github.com>
2023-10-02 18:16:45 +0500

    w

--- Controllers/errorControllers.js
+++ Controllers/errorControllers.js
@@ -4,6 +4,7 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+//
 const a = 1;
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
